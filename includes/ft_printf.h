3c6bfb03d8ddac1df6a6ed3e889c5e0f624d14f7
mlanca-c <79856624+mlanca-c@users.noreply.github.com>
2021-03-11 18:50:05 +0000

    1st try - Update includes

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 14:54:58 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/03/05 18:58:25 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/03/11 11:58:58 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define FT_PRINTF_H
 
 # include "../libft/libft.h"
-# include "stdarg.h"
+# include <stdarg.h>
 # include <stdio.h>
 typedef struct	s_flags
 {
@@ -27,17 +27,17 @@ typedef struct	s_flags
 	char	type;
 }				t_flags;
 
-int		ft_printf(const char *fmt, ...);
-int		set_params(char *input, t_flags *flags, va_list args);
-int		get_case(t_flags *flags, va_list args);
-int		case_c(t_flags *flags, va_list args);
-int		case_s(t_flags *flags, va_list args);
-int		case_p(t_flags *flags, va_list args);
-int		case_d(t_flags *flags, va_list args);
-int		case_i(t_flags *flags, va_list args);
-int		case_u(t_flags *flags, va_list args);
-int		case_x(t_flags *flags, va_list args);
-int		case_upperx(t_flags *flags, va_list args);
-int		case_percentage(t_flags *flags);
+int				ft_printf(const char *fmt, ...);
+int				set_params(char *input, t_flags *flags, va_list args);
+int				get_case(t_flags *flags, va_list args);
+int				case_c(t_flags *flags, va_list args);
+int				case_s(t_flags *flags, va_list args);
+int				case_p(t_flags *flags, va_list args);
+int				case_d(t_flags *flags, va_list args);
+int				case_i(t_flags *flags, va_list args);
+int				case_u(t_flags *flags, va_list args);
+int				case_x(t_flags *flags, va_list args);
+int				case_upperx(t_flags *flags, va_list args);
+int				case_percentage(t_flags *flags);
 
 #endif
