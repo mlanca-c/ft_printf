6287d6576b3749c989070177f6d42865488d68e3
mlanca-c <79856624+mlanca-c@users.noreply.github.com>
2021-03-15 18:44:38 +0000

    1st try - Update ft_printf
    mandatory:
    | case c: OK | case s: OK | case p: OK | case d: OK | case i: OK | case u: OK | case x: OK | case X: OK | case %: OK | 
    bonus:
    | case n: OK | case f: OK | case e: OK | case g: OK |
    | case space: OK | case +: OK | case #: KO |
    | case l: KO | case ll: OK | case h: OK | case hh: OK |

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 15:03:39 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/03/13 18:03:35 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/03/15 15:29:18 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,11 @@ void	print_flags(t_flags *flags)
 	printf("width: %d\n", flags->min_width);
 	printf("point: %d\n", flags->point);
 	printf("precision: %d\n", flags->precision);
+	printf("hash: %d\n", flags->hash);
+	printf("space: %d\n", flags->space);
+	printf("plus: %d\n", flags->plus);
+	printf("l: %d\n", flags->l);
+	printf("h: %d\n", flags->h);
 	printf("type: %c\n", flags->type);
 }
 
@@ -34,6 +39,11 @@ t_flags	init(void)
 	flags.min_width = 0;
 	flags.point = 0;
 	flags.precision = 0;
+	flags.hash = 0;
+	flags.space = 0;
+	flags.plus = 0;
+	flags.l = 0;
+	flags.h = 0;
 	flags.type = 0;
 	return (flags);
 }
@@ -54,8 +64,8 @@ int		check_format(char *input, va_list args)
 		if (input[i] == '%' && input[i + 1])
 		{
 			i += set_params(&input[i], &flags, args);
-			count += get_case(&flags, args);
-		//	print_flags(&flags);
+			count += get_case(&flags, args, &count);
+			//print_flags(&flags);
 		}
 		else
 			count += ft_putchar(input[i]);
