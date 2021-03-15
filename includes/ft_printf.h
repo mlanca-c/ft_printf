6b6477bf634c5dc011897937ce6a63196e79e204
mlanca-c <79856624+mlanca-c@users.noreply.github.com>
2021-03-15 18:44:12 +0000

    1st try - Update includes
    mandatory:
    | case c: OK | case s: OK | case p: OK | case d: OK | case i: OK | case u: OK | case x: OK | case X: OK | case %: OK | 
    bonus:
    | case n: OK | case f: OK | case e: OK | case g: OK |
    | case space: OK | case +: OK | case #: KO |
    | case l: KO | case ll: OK | case h: OK | case hh: OK |

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,12 +6,18 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 14:54:58 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/03/13 20:41:28 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/03/15 18:21:07 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
+
+//PROVAVELMENTE NAO POSSO FAZER ISTO
+# define D "0123456789"
+# define H "0123456789abcdef"
+# define UH "0123456789ABCDEF"
+# define O "01234567"
 
 # include "../libft/libft.h"
 # include <stdarg.h>
@@ -24,12 +30,22 @@ typedef struct	s_flags
 	int		min_width;
 	int		point;
 	int		precision;
+	int		hash;
+	int		space;
+	int		plus;
+	int		l;
+	int		h;
 	char	type;
 }				t_flags;
 
 int				ft_printf(const char *fmt, ...);
 int				set_params(char *input, t_flags *flags, va_list args);
-int				get_case(t_flags *flags, va_list args);
+void			handle_star(t_flags *flags, int arg);
+int				handle_digit(t_flags *flags, char *input);
+void			handle_minus(t_flags *flags);
+void			handle_flags(char input, t_flags *flags, va_list args);
+int				handle_lh(char *input, t_flags *flags);
+int				get_case(t_flags *flags, va_list args, int *count);
 int				case_c(t_flags *flags, va_list args);
 int				case_s(t_flags *flags, va_list args);
 int				case_p(t_flags *flags, va_list args);
@@ -38,9 +54,13 @@ int				case_i(t_flags *flags, va_list args);
 int				case_u(t_flags *flags, va_list args);
 int				case_x(t_flags *flags, va_list args);
 int				case_percentage(t_flags *flags);
+int				case_n(t_flags *flags, va_list args, int *count);
+int				case_f(t_flags *flags, va_list args);
+int				case_g(t_flags *flags, va_list args);
+int				case_e(t_flags *flags, va_list args);
+int				case_o(t_flags *flags, va_list args);
 int				handle_width(t_flags *flags, int len);
 char			*arg_conversions(t_flags *flags, va_list args);
-char			*handle_negative(t_flags *flags, char *nbr);
 char			*handle_number(t_flags *flags, char *nbr);
 
 #endif
